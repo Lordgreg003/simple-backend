0effa3f7cc0a2c1adf7a253fd013815957af86e5
chidera nwokoye <chideranwokoye555@gmail.com>
2024-09-02 13:44:42 +0100

    recent changes

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
+
+const app = express();
+
 const dotenv = require("dotenv");
+dotenv.config();
+
 const cors = require("cors");
 const connectDB = require("./config/db");
-
-dotenv.config();
+connectDB();
 
 console.log("PORT:", process.env.PORT);
 console.log("MONGO_URI:", process.env.MONGO_URI);
-const app = express();
-connectDB();
+
 const taskRoutes = require("./routes/taskRoutes");
 const healthRoutes = require("./routes/healthRoutes");
 
@@ -19,6 +22,15 @@ app.use(cors());
 app.use("/api/v1", taskRoutes);
 
 app.use("/api/v1", healthRoutes);
+
+// Error Middlewares
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+//Not found URL middleware
+app.use(notFound);
+
+//Error handler for the whole app
+app.use(errorHandler);
 
 // Start the server
 const port = process.env.PORT || 3000;
